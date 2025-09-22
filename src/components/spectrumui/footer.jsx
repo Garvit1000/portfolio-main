e7471b67cb9c17d690766dd51d414d101d12dfac
Garvit1000 <garvitjoshi543@gmail.com>
2025-09-22 10:05:12 +0530

    add:Footer text file

---
+++ src/components/spectrumui/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function FooterText() {
+  return (
+    <footer className=" py-12 px-4 md:px-6 z-50">
+      <div className="container mx-auto">
+        <div className=" w-full flex mt-4 items-center justify-center   ">
+          <h1
+            className="text-center text-3xl md:text-5xl lg:text-[10rem] font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-700 to-neutral-900 select-none">
+            GARVITJOSHI
+          </h1>
+        </div>
+      </div>
+    </footer>
+  );
+}
